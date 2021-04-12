683437fb74d31dec41d97c4acf1664394d94d18d
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2021-04-12 15:48:46 +0200

    first

---
+++ src/test/java/com/example/demodecoratequeues/DemoDecorateQueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodecoratequeues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDecorateQueuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
